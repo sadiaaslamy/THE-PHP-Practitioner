b5baedcde2b267b0d69176fdc37b2aeaf71da7e1
Sadia Aslamy <62790622+sadiaaslamy@users.noreply.github.com>
2022-11-28 01:50:47 +0100

    Add files via upload

---
+++ router/views/index.view.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP learning</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="/about">About</a></li>
+            <li><a href="/about/culture">About our Culture</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </nav>
+
+    <h1>My Tasks</h1>
+        <?php foreach ($tasks as $task): ?>
+            <li>
+                <?php if($task->completed) : ?>
+
+                    <strike><?= $task->description; ?></strike>
+
+                <?php else: ?>
+
+                    <?= $task->description; ?>
+                    
+                <?php endif; ?>    
+            </li>
+        <?php endforeach; ?>
+   
+
+  
+</body>
+</html>
