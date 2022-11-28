43cacf5edad91f7c62ab8de03bebfb236e3f2caf
Sadia Aslamy <62790622+sadiaaslamy@users.noreply.github.com>
2022-11-28 01:46:31 +0100

    Add files via upload

---
+++ 11.Hide your password/config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+return [
+    'database' => [
+        'name' => 'mytodo',
+        'username' => 'root',
+        'password' => 'root',
+        'connection' => 'mysql:host=127.0.0.1',
+        'options' => []
+    ] 
+    ];
